9d29c30b41483a2a55c254ccf79add9b0e9dadb2
SadHappy <craigjm19@gmail.com>
2025-05-31 11:34:13 +0100

    Added setup.py to allow pip installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="en_words",
+    packages=find_packages(),
+    author="Craig Mason",
+    description="A package for working with English dictionaries and finding words and anagrams.",
+    url="https://github.com/CraigMason19/en_words",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
