50318bb0df154e05591d79bef3b3b48a915eeb19
SadHappy <craigjm19@gmail.com>
2025-06-06 09:38:00 +0100

    added easier imports to the package

---
+++ en_words/__init__.py
@@ -0,0 +1,23 @@
+from en_words.en_words import (
+    create_sorted_dict, 
+    unsorted_words, 
+    unsorted_words_gen, 
+    sorted_words, 
+    sorted_words_gen, 
+    word_count, 
+    find_largest_word, 
+    words_of_length, 
+    words_of_length_gen, 
+    is_potential_match, 
+    potential_words, 
+    letters_in_word, 
+    words_from_letters, 
+    anagrams, 
+    anagrams_gen)
+
+# from en_words.word_games import (
+#     spelling_bee, 
+#     wordle, 
+#     polygon, 
+#     cash_square, 
+#     countdown)
