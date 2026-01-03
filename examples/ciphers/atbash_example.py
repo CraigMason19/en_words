2cb10ad41fe0279e8b6e3bc9700917a6a028a366
SadHappy <craigjm19@gmail.com>
2026-01-03 14:23:14 +0000

    added example

---
+++ examples/ciphers/atbash_example.py
@@ -0,0 +1,22 @@
+from ciphers.atbash import Atbash
+
+
+def atbash_example():
+    print("Atbash Cipher Example")
+
+    original = "Hello World!"
+    encoded = Atbash.encode(original)
+    decoded = Atbash.decode(encoded)
+
+    print("\nOriginal message:")
+    print(f"\t{original}")
+
+    print("\nEncoded message:")
+    print(f"\t{encoded}")
+
+    print("\nDecoded message:")
+    print(f"\t{decoded}")
+
+
+if __name__ == "__main__":
+    atbash_example()
