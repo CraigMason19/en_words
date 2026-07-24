7cb502502386c469b5ff0202846d36eaa76758e6
SadHappy <craigjm19@gmail.com>
2026-07-24 16:03:30 +0100

    Updated minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="en_words",
-    version="1.11.1",
+    version="1.12.0",
     packages=find_packages(),
     author="Craig Mason",
     description="A package for working with English dictionaries and finding words and anagrams. Also contains a range of ciphers for working with strings",
