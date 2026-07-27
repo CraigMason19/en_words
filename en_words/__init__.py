6b32f520f48f58a80b091d39956a25914a62db91
SadHappy <craigjm19@gmail.com>
2026-07-27 17:15:42 +0100

    en_words into includes modules rather than functions

--- en_words/__init__.py
+++ en_words/__init__.py
@@ -1,35 +1,40 @@
-from en_words.en_words import (
-    create_sorted_dict, 
-    unsorted_words, 
-    unsorted_words_gen, 
-    sorted_words, 
-    sorted_words_gen, 
-    word_count, 
-    find_largest_word, 
-    words_of_length, 
-    words_of_length_gen, 
-    is_potential_match, 
-    potential_words, 
-    letters_in_word, 
-    words_from_letters, 
-    anagrams, 
-    anagrams_gen)
+from . import en_words
+from . import letters
+from . import NATO
+from . import word_games
 
-from en_words.letters import (
-	letter_to_index,
-	index_to_letter,
-	next_letter,
-	previous_letter,
-	shift_letter_up,
-	shift_letter_down,
-	shift,
-	alphabet_generator,
-	shift_alphabet,
-	caesar_cipher,
-	random_lower_letter,
-	random_upper_letter,
-	random_letter
-)
+# from en_words.en_words import (
+#     create_sorted_dict, 
+#     unsorted_words, 
+#     unsorted_words_gen, 
+#     sorted_words, 
+#     sorted_words_gen, 
+#     word_count, 
+#     find_largest_word, 
+#     words_of_length, 
+#     words_of_length_gen, 
+#     is_potential_match, 
+#     potential_words, 
+#     letters_in_word, 
+#     words_from_letters, 
+#     anagrams, 
+#     anagrams_gen)
+
+# from en_words.letters import (
+# 	letter_to_index,
+# 	index_to_letter,
+# 	next_letter,
+# 	previous_letter,
+# 	shift_letter_up,
+# 	shift_letter_down,
+# 	shift,
+# 	alphabet_generator,
+# 	shift_alphabet,
+# 	caesar_cipher,
+# 	random_lower_letter,
+# 	random_upper_letter,
+# 	random_letter
+# )
 
 # from en_words.word_games import (
 #     spelling_bee, 
